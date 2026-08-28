370b81ea982f5c9a9eb96ac532c052787ebe6c43
jyl9311 <jyl9311@gmail.com>
2026-08-28 10:44:54 +0900

    fix: Entity 값 저장 시 null 체크

--- src/main/java/com/bikeridediary/domain/bikemodel/service/BikeModelInsertService.java
+++ src/main/java/com/bikeridediary/domain/bikemodel/service/BikeModelInsertService.java
@@ -21,25 +21,22 @@ public class BikeModelInsertService {
         String displacement = node.path("displacement").asText(null);
 
         String engine = node.path("engine").asText(null);
-        engine = translateEngine(engine);
+        engine = engine == null ? null : translateEngine(engine);
 
         String fuelCapacity = node.path("fuel_capacity").asText(null);
-        fuelCapacity = fuelCapacity.substring(0, fuelCapacity.indexOf(" lit"));
+        fuelCapacity = fuelCapacity == null ? null : fuelCapacity.substring(0, fuelCapacity.indexOf(" lit"));
 
 
         String power = node.path("power").asText(null);
-        power = power.substring(0, power.indexOf("HP"));
-        power += "마력";
+        power = power == null ? null : power.substring(0, power.indexOf("HP")) + "마력";
 
         String torque = node.path("torque").asText(null);
 
         String totalWeight = node.path("total_weight").asText(null);
-        int i = totalWeight.indexOf("kg");
-        totalWeight = totalWeight.substring(0, i + 2);
+        totalWeight = totalWeight == null ? null : totalWeight.substring(0, totalWeight.indexOf("kg") + 2);
 
         String seatHeight = node.path("seat_height").asText(null);
-        int j = seatHeight.indexOf("mm");
-        seatHeight = seatHeight.substring(0, j + 2);
+        seatHeight = seatHeight == null ? null : seatHeight.substring(0, seatHeight.indexOf("mm") + 2);
 
         bikeModelRepository.save(BikeModelEntity.create(
                 manufacturer,
